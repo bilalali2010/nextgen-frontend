61245f3e120bd314688211604f818ba2015ab62b
bilalali2010 <bilalali0905@gmail.com>
2026-01-26 14:41:18 +0500

    Update and rename pdf.js to pdf.ts

--- lib/pdf.ts
+++ lib/pdf.ts
@@ -1,9 +1,9 @@
 import * as pdfjsLib from "pdfjs-dist";
 import workerSrc from "pdfjs-dist/build/pdf.worker.min.js";
 
-pdfjsLib.GlobalWorkerOptions.workerSrc = workerSrc;
+(pdfjsLib as any).GlobalWorkerOptions.workerSrc = workerSrc;
 
-export async function extractPDFText(file) {
+export async function extractPDFText(file: File): Promise<string> {
   const buffer = await file.arrayBuffer();
   const pdf = await pdfjsLib.getDocument({ data: buffer }).promise;
 
@@ -12,7 +12,7 @@ export async function extractPDFText(file) {
   for (let i = 1; i <= pdf.numPages; i++) {
     const page = await pdf.getPage(i);
     const content = await page.getTextContent();
-    text += content.items.map(i => i.str).join(" ") + "\n";
+    text += content.items.map((item: any) => item.str).join(" ") + "\n";
   }
 
   return text;
